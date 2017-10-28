0d69960215c3ce76e8fae0390c4d0b71789a0b14
Tanel Hiob <tanelhiob@outlook.com>
2017-10-28 20:26:45 +0300

    Refactored

--- Ocugo9000/Ocugo9000.py
+++ Ocugo9000/Ocugo9000.py
@@ -8,107 +8,113 @@ import numpy as np
 #VIOLET 160
 #RED 179
 
-kernel = np.ones((5,5),np.uint8)
-lower = np.array([50,50,0])
-upper = np.array([95,255,255])
-doProcessing = True
+isPi = False
+
+red = (0, 0, 255)
+green = (0, 255, 0)
 
 
-lower2 = np.array([0,0,5])
-upper2 = np.array([179,255,255])
-shape = cv2.imread('./sabloon.jpg')
-hsv = cv2.cvtColor(shape, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsv, lower2, upper2)
-res2 = cv2.bitwise_and(shape,shape, mask= mask)
+def createTemplateShape ():
 
-res2 = cv2.erode(res2,kernel,iterations = 1)
-res2 = cv2.dilate(res2,kernel,iterations = 1)
-shapeGray = cv2.cvtColor(res2,cv2.COLOR_BGR2GRAY)
-#thresh = cv2.threshold(shapeGray,127,255,cv2.THRESH_BINARY)
-_, cntCompare, _ = cv2.findContours(shapeGray,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	original = cv2.imread('./sabloon.jpg')
 
-cap = cv2.VideoCapture(0)
+	image = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
+	lower = np.array([0, 0, 5])
+	upper = np.array([179, 255, 255])
+	mask = cv2.inRange(image, lower, upper)
+	image = cv2.bitwise_and(image, image, mask = mask)
+	kernel = np.ones((5,5),np.uint8)
+	image = cv2.erode(image,kernel,iterations = 1)
+	image = cv2.dilate(image,kernel,iterations = 1)
+	image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-def getGreenLight (cap):
+	if isPi:
+		contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	else:
+		_, contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 	
-	# Take each frame
-	_, frame = cap.read()
+	#debug
+	#cv2.drawContours(original,contours[0],-1,(0,0,255),1)
+	#cv2.imshow('contours', original)
 
-	#frame = cv2.imread('./foor-4.jpeg')
+	if not len(contours) == 1:
+		raise("template doesn't have exactly 1 contours, actual result: " + str(len(contours)))
 
-	# Convert BGR to HSV
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	return contours[0];
 
-	# define range of blue color in HSV
-	# hue, sat, bright
-	#lower = np.array([70,85,50])
-	#upper = np.array([95,255,255])
+def processFrameForCamera(frame):
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	lower = np.array([70,150,50])
 	upper = np.array([95,255,255])
+	mask = cv2.inRange(frame, lower, upper)
+	frame = cv2.bitwise_and(frame, frame, mask = mask)
+	kernel = np.ones((2, 2), np.uint8)
+	frame = cv2.erode(frame, kernel, iterations = 1)
+	frame = cv2.dilate(frame, kernel, iterations = 1)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	return frame
 
-	# Threshold the HSV image to get only specified colors
-	mask = cv2.inRange(hsv, lower, upper)
+def processFrameForTrainingImages(frame):
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower = np.array([70,150,50])
+	upper = np.array([95,255,255])
+	mask = cv2.inRange(frame, lower, upper)
+	frame = cv2.bitwise_and(frame, frame, mask = mask)
+	frame = cv2.medianBlur(frame, 1)
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	return frame
 
-	# Bitwise-AND mask and original image
-	res = cv2.bitwise_and(frame,frame, mask= mask)
+def getGreenLight (original, templateShape, processingFunction):
 
-	kernel2 = np.ones((2,2),np.uint8)
-	res = cv2.erode(res,kernel2,iterations = 1)
-	res = cv2.dilate(res,kernel2,iterations = 1)
+	frame = processingFunction(original)
 
-	res = cv2.medianBlur(res, 1)
-
-	grey = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
-	_, cntCam, _ = cv2.findContours(grey,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	if isPi:
+		contours, _ = cv2.findContours(frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+	else:
+		_, contours, _ = cv2.findContours(frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 	
-	#print(len(cntCam))
-
-	probableContours = list(filter(lambda x: x.size > 25, cntCam))
-
-	matches = list(map(lambda x: (x, cv2.matchShapes(x, cntCompare[0], 1, 0.0)), probableContours))
-	#match = cv2.matchShapes(cntCam[0],cntCompare[0],1,0.0)
-	#print(matches)
-
+	probableContours = list(filter(lambda x: x.size > 15, contours))
+	matches = list(map(lambda x: (x, cv2.matchShapes(x, templateShape, 1, 0.0)), probableContours))
 	matches.sort(key = lambda x: x[1])
 	
-
-	red = (0, 0, 255)
-	green = (0, 255, 0)
-
-
-	#colored = cv2.circle(median,(25,25), 25, green, -1)
-
-	#cv2.drawContours(shape,cntCompare,-1,(0,0,255),1)
 	if len(matches) > 0:
+
+		#debug
 		print(matches[0][1])
-		cv2.drawContours(res, matches[0][0],-1,(0,0,255),1)
-	cv2.drawContours(shape,cntCompare,-1,(0,0,255),1)
-	#cv2.imshow('shape', shape)
-	cv2.imshow('frame', frame)
-	cv2.imshow('res', res)
+		bestContours = list(map(lambda x: x[0], matches[:1]))
+		cv2.drawContours(original, bestContours,-1,(0,0,255),1)
+		cv2.imshow('frame', original)
 
-	#cv2.imshow('frame',frame)
-	#cv2.imshow('new',newimg)
-	#cv2.imshow('mask',mask)
-	#cv2.imshow('compare',shape)
-	#cv2.imshow('res',colored)
+		if matches[0][1] < 0.5:
+			return True
+		else:
+			return False
+	else:
+		return False
 
-	return False
 
 def actionOnGreenLight ():
 	return
 
-def actionOntFound ():
+
+def actionOnNotFound ():
 	return
 
-while(doProcessing):
 
-	isGreenLight = getGreenLight(cap)
+cap = cv2.VideoCapture(0)
+templateShape = createTemplateShape()
+
+while(True):
+
+	_, frame = cap.read()
+	isGreenLight = getGreenLight(frame, templateShape, processFrameForCamera)
+
+	#isGreenLight = getGreenLight(cv2.imread('TrainingImages\\foor-1.jpeg'), templateShape, processFrameForTrainingImages)
 
 	if isGreenLight:
 		actionOnGreenLight()
 	else:
-		actionOntFound()
+		actionOnNotFound()
 
 	k = cv2.waitKey(5) & 0xFF
 	if k == 27:
